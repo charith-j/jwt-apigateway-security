eb715996e1d4caeb869d9d546a91738819116f5b
Charith Jayasekara <charithjayasekara@192.168.1.6>
2023-10-29 21:15:25 +0530

    repository edited

--- swiggy-gateway/src/main/java/com/javatechie/SwiggyGatewayApplication.java
+++ swiggy-gateway/src/main/java/com/javatechie/SwiggyGatewayApplication.java
@@ -3,6 +3,11 @@ package com.javatechie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +16,19 @@ public class SwiggyGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SwiggyGatewayApplication.class, args);
 	}
+	
+//	@Bean
+//	public CorsWebFilter corsWebFilter() {
+//	    CorsConfiguration corsConfig = new CorsConfiguration();
+//	    corsConfig.addAllowedOrigin("http://localhost:3000");
+//	    corsConfig.addAllowedMethod("*");
+//	    corsConfig.addAllowedHeader("*");
+//	    corsConfig.setAllowCredentials(true);
+//
+//	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	    source.registerCorsConfiguration("/**", corsConfig);
+//
+//	    return new CorsWebFilter(source);
+//	}
 
 }
